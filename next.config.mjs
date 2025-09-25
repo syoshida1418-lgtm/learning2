b9773dc55d698004d4b3abebdd085001c95c62a0
syoshida1418-lgtm <syoshida1418@gmail.com>
2025-09-24 17:24:27 -0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import withPWA from '@ducanh2912/next-pwa'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+const withPWAConfig = withPWA({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})
+
+export default withPWAConfig(nextConfig)
